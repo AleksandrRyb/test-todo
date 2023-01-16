cc676d689dd51533baa96d4cba0a411f0ef483ef
StroitochkaProg <prog3@yumkom.ru>
2023-01-16 23:18:54 +0300

    Черновик по выводу данных туду листов

--- src/components/todo-window/todo-window.tsx
+++ src/components/todo-window/todo-window.tsx
@@ -1,10 +1,11 @@
 import React from "react";
 import { useQuery } from "react-query";
-import { Flex, Box, Heading, Button } from "@chakra-ui/react";
+import { Flex, Box, Heading, Button, Stack, Skeleton } from "@chakra-ui/react";
 import { SettingsIcon } from "@chakra-ui/icons";
 import { getTodos } from "../../api/queries";
 
 import AddTodoForm from "../../ui-kit/todo-item-form";
+import TodosAccordion from "../../ui-kit/todos-accordion/todos-accordion";
 
 const TodoWindow = () => {
   const [isOpen, setIsOpen] = React.useState(false);
@@ -19,25 +20,41 @@ const TodoWindow = () => {
   return (
     <Box
       width="390px"
-      height="auto"
+      height="500px"
       minHeight="200px"
       paddingX="20px"
       paddingY="13px"
       position="relative"
       bg="gray.100"
-      borderRadius="30px"
+      borderTopRadius="30px"
     >
-      <Flex paddingX="17px" alignItems="center" justifyContent="space-between">
+      <Flex
+        mb="15px"
+        paddingX="17px"
+        alignItems="center"
+        justifyContent="space-between"
+      >
         <Heading fontSize="36px" as="h1">
           To Do
         </Heading>
         <SettingsIcon width="28.5px" height="30px" />
       </Flex>
+
+      {query.isLoading && (
+        <Stack>
+          <Skeleton borderRadius="25px" height="79px" />
+          <Skeleton borderRadius="25px" height="79px" />
+          <Skeleton borderRadius="25px" height="79px" />
+        </Stack>
+      )}
+
+      {query.data && <TodosAccordion todosBundles={query.data} />}
+
       <Button
         onClick={() => setIsOpen(!isOpen)}
         position="absolute"
         right="0"
-        bottom="0"
+        bottom="-40px"
         borderRadius="none"
         borderBottomRadius="36px"
         width="100%"
--- src/ui-kit/todos-accordion/todos-accordion.tsx
+++ src/ui-kit/todos-accordion/todos-accordion.tsx
@@ -1,16 +1,68 @@
-import { Accordion } from "@chakra-ui/react";
+import {
+  Accordion,
+  AccordionButton,
+  AccordionIcon,
+  AccordionItem,
+  AccordionPanel,
+  Box,
+  Flex,
+  Heading,
+  List,
+  ListItem,
+  Switch,
+  Text,
+} from "@chakra-ui/react";
+import { v4 as uuidv4 } from "uuid";
 import type { ITodo } from "../../api/mutations";
 
 interface ITodosAccordion {
-  accordionTitle: string;
-  accordionItems: ITodo[];
+  todosBundles: { [key: string]: ITodo[] };
 }
 
-const TodosAccordion = ({
-  accordionTitle,
-  accordionItems,
-}: ITodosAccordion) => {
-  return <Accordion></Accordion>;
+const TodosAccordion = ({ todosBundles }: ITodosAccordion) => {
+  return (
+    <Accordion allowMultiple>
+      {Object.keys(todosBundles).map((todoBundleKey) => (
+        <AccordionItem border="none" key={todoBundleKey}>
+          <Heading as="h2">
+            <AccordionButton>
+              <Flex display="flex" justifyContent="space-between">
+                <Box w="5px" bg="gray.500" />
+                <Box marginRight="20px">{todoBundleKey}</Box>
+                <AccordionIcon />
+              </Flex>
+            </AccordionButton>
+          </Heading>
+          <AccordionPanel>
+            <List>
+              {todosBundles[todoBundleKey].map((todo) => (
+                <ListItem mb="20px" key={todo.id}>
+                  <Flex>
+                    <Box
+                      mr="10px"
+                      borderRadius="3px"
+                      w="5px"
+                      bg={todo.colorBadge}
+                    />
+                    <Flex flex="8" flexDirection="column">
+                      <Heading as="h3">{todo.title}</Heading>
+                      <Text>{todo.description}</Text>
+                    </Flex>
+                    <Switch
+                      display="flex"
+                      alignItems="center"
+                      size="md"
+                      colorScheme="blue"
+                    />
+                  </Flex>
+                </ListItem>
+              ))}
+            </List>
+          </AccordionPanel>
+        </AccordionItem>
+      ))}
+    </Accordion>
+  );
 };
 
 export default TodosAccordion;
